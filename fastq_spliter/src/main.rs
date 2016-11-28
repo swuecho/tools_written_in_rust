63cfd18c4b9aa528c86364cf4a05bd3958192db6
Hao Wu <hwu@intellisurvey.com>
2016-11-27 19:11:29 -0800

    clean

--- fastq_spliter/src/main.rs
+++ fastq_spliter/src/main.rs
@@ -9,14 +9,8 @@ fn filter_records(in_file: &str, out_file: &str) {
     let reader = fastq::Reader::from_file(in_file).unwrap();
     let mut writer = fastq::Writer::to_file(out_file).unwrap();
     let mut rng = thread_rng();
-    // let records: Vec<io::Result<Record>> = reader.records().collect();
     for result in reader.records() {
-        // obtain record or fail with error
         let record = result.unwrap();
-        // obtain sequence
-        // let seq = record.seq();
-        // let s = String::from_utf8(seq).expect("Found invalid UTF-8");
-        // random bool
         if rng.gen() {
             writer.write_record(&record).unwrap();
         }
