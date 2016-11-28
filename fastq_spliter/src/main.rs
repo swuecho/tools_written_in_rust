d13a97096046f78f89220b1a6bd7aba22067e8ab
Hao Wu <hwu@intellisurvey.com>
2016-11-27 16:21:33 -0800

    update

--- fastq_spliter/src/main.rs
+++ fastq_spliter/src/main.rs
@@ -5,14 +5,7 @@ use bio::io::fastq;
 use rand::Rng;
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() != 2 {
-        println!("error: 2 and only 2 args required");
-        return;
-    }
-    let in_file = &args[0];
-    let out_file = &args[1];
+fn filter_records(in_file: &str, out_file: &str) {
     let reader = fastq::Reader::from_file(in_file).unwrap();
     let mut writer = fastq::Writer::to_file(out_file).unwrap();
     let mut rng = thread_rng();
@@ -28,4 +21,14 @@ fn main() {
             writer.write_record(&record).unwrap();
         }
     }
+}
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 2 {
+        println!("error: 2 and only 2 args required");
+        return;
+    }
+    let in_file = &args[0];
+    let out_file = &args[1];
+    filter_records(in_file, out_file)
 }
