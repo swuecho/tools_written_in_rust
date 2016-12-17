63e091445fd0f92835d70c77623d0310ed5f63f7
Hao Wu <echowuhao@gmail.com>
2016-12-17 15:37:17 -0800

    update

--- zsh_history/src/main.rs
+++ zsh_history/src/main.rs
@@ -1,9 +1,11 @@
 extern crate time;
+
+use std::env;
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
 use std::collections::HashMap;
-use std::env;
+
 fn main() {
 
     let path = env::home_dir().unwrap().join(".zsh_history");
@@ -12,15 +14,14 @@ fn main() {
     let mut data_map = HashMap::new();
     let current_time_unix = time::get_time().sec as i32;
     for line in reader.lines() {
-        if line.is_ok() {
-            let line_content = line.unwrap();
+        if let Ok(line_content) = line {
             let time_and_cmd: Vec<&str> = line_content.split(":").collect(); //.unwrap().map(to_string); //map(ToOwned::to_owned);
             if time_and_cmd.len() > 2 {
                 let time = time_and_cmd[1];
                 let cmd = time_and_cmd[2];
-                let unix_time_parsed = (&time[1..]).parse::<i32>();
-                if unix_time_parsed.is_ok() {
-                    if current_time_unix - unix_time_parsed.unwrap() < 60 * 60 * 24 {
+                let unix_time_parsed_result = (&time[1..]).parse::<i32>();
+                if let Ok(unix_time_parsed) = unix_time_parsed_result {
+                    if current_time_unix - unix_time_parsed < 60 * 60 * 24 {
                         // println!("{}", &cmd[2..].to_string());
                         let cmd_clean = (&cmd[2..]).to_string();
                         *data_map.entry(cmd_clean).or_insert(0) += 1; // why * is nccessayr
@@ -29,7 +30,7 @@ fn main() {
             }
         }
     }
-    for (contact, &number) in data_map.iter() {
-        println!("{}: {}", number, contact);
+    for (cmd, &times) in data_map.iter() {
+        println!("{}: {}", cmd, times);
     }
 }
